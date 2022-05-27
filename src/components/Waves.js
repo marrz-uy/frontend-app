7220b2d68a6f76566ebc8dde615714481c5856e6
martinrodfl <martinrodf@gmail.com>
2022-05-27 16:42:23 -0300

    Creando ramas dev y stage

--- src/components/Waves.js
+++ src/components/Waves.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import imageFu from '../assets/loginPerson.png';
 import '../App.css';
 
 const Waves = () => {
   return (
     <div className="waves">
-      <img src={imageFu} alt="App" id="app" />
+      
     </div>
   );
 };
