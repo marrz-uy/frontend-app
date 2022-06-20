939c33b32fe69451015ceb63478b5d36d63f42da
Ignacio <mateunacho6@gmail.com>
2022-06-20 08:45:30 -0300

    FL-37

--- src/Pages/Principal.jsx
+++ src/Pages/Principal.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Layout } from '../Layout';
 import '../Css/Principal.css';
 import hotelImg from '../Assets/categoriesImages/hospedaje.png';
@@ -7,8 +6,16 @@ import setYourTour from '../Assets/categoriesImages/mosaico.png';
 import restaurant from '../Assets/categoriesImages/fast-food 1.png';
 import trips from '../Assets/categoriesImages/summer-holidays 1.png';
 import transport from '../Assets/categoriesImages/bus.png';
+import { useNavigate } from 'react-router-dom';
 
-const Principal = ({ text }) => {
+const Principal = ({ setItems }) => {
+  const navigate = useNavigate();
+
+  const handleCategories = (e) => {
+    setItems(e)
+    navigate('/results');
+  }
+
   return (
     <Layout>
       <div className="container">
@@ -16,18 +23,18 @@ const Principal = ({ text }) => {
           <img className="lacarretera" src={predefTour} alt="hotel"></img>
         </div>
         <div className="categories">
-          <img src={setYourTour} alt="hotel"></img>
+          <img src={setYourTour} alt="hotel" ></img>
         </div>
-        <div className="categories">
+        <div className="categories" onClick={() => handleCategories('hoteles')}>
           <img src={hotelImg} alt="hotel"></img>
         </div>
-        <div className="categories">
+        <div className="categories" onClick={() => handleCategories('restaurantes')}>
           <img src={restaurant} alt="hotel"></img>
         </div>
-        <div className="categories">
+        <div className="categories" onClick={() => handleCategories('paseos')}>
           <img src={trips} alt="hotel"></img>
         </div>
-        <div className="categories">
+        <div className="categories" onClick={() => handleCategories('transportes')}>
           <img src={transport} alt="hotel"></img>
         </div>
       </div>
