0b4c9043e5dad9b0b356f438f53ff5ad1142d040
martinrodfl <martinrodf@gmail.com>
2022-08-16 13:16:27 -0300

    Se traen datos de backoffice OK/ mejorado por tipo de servicios_esenciales

--- src/Pages/SearchResults.jsx
+++ src/Pages/SearchResults.jsx
@@ -4,7 +4,7 @@ import '../Css/SearchResults.css';
 import ResultsCard from '../Components/ResultsCard';
 
 const SearchResults = ({ items, setPage, text }) => {
-  console.log('%cITEMS search results:', 'color: orange;', items);
+  // console.log('%cITEMS search results:', 'color: orange;', items);
   useEffect(() => {
     setPage('results');
   }, [setPage]);
@@ -13,12 +13,14 @@ const SearchResults = ({ items, setPage, text }) => {
     <Layout>
       <div className="results ">
         <h6 className="resultsText">
-          {!items || items.length === 0 ? '0 Resultados' : `${items.length} Resultados para ${text}`} 
+          {!items || items.length === 0
+            ? '0 Resultados'
+            : `${items.length} Resultados para ${text}`}
         </h6>
         <div className="infoResults">
           {!items ? (
             <div className="sinResultado">
-              <p>Lo sentimos,intente otra busqueda por favor</p>
+              <p>Lo sentimos, intente otra busqueda por favor</p>
             </div>
           ) : (
             items.map((item) => {
