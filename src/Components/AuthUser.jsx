b3d48dc4f3aa1f9864c1b89c3619658b2034acf7
martinrodfl <martinrodf@gmail.com>
2022-08-28 19:31:11 -0300

    Probar paginado

--- src/Components/AuthUser.jsx
+++ src/Components/AuthUser.jsx
@@ -66,7 +66,7 @@ export default function AuthUser() {
   };
 
   const http = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://f71b-2800-a4-16c9-cd00-903f-5ae4-76d0-e9b9.sa.ngrok.io/api',
     headers: {
       'Content-type': 'application/json',
       Authorization: `Bearer ${token}`,
