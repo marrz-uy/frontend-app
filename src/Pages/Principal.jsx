e285482484d178b7f6271455f7adf758fb65ec9c
martinrodfl <martinrodf@gmail.com>
2022-09-01 19:41:12 -0300

    Ver Espectaculos Frontend/ok y mejorar paginacion/ ok

--- src/Pages/Principal.jsx
+++ src/Pages/Principal.jsx
@@ -14,16 +14,23 @@ import actividaesNocturnas from '../Assets/categoriesImages/cocktail 1.png';
 import serviciosEscenciales from '../Assets/categoriesImages/services 1.png';
 import serviciosInfantiles from '../Assets/categoriesImages/calesita 1.png';
 import { useNavigate } from 'react-router-dom';
+import useScreenSize from '../Helpers/ScreenSize';
 
-const Principal = ({ setItems,  items,  setPage, setText }) => {
+const Principal = ({ setItems, items, setPage, setText }) => {
   const { textos } = useContext(LenguageContext);
   const [seeAll, setSeeAll] = useState(false);
   const [btnText, setBtnText] = useState('');
+
+  const {width} = useScreenSize()
+
+  console.log('WIDTH: ', width)
+  
+
   useEffect(() => {
     setPage('principal');
   }, [setPage]);
 
-  console.log('BTNTXT: ', btnText)
+  console.log('BTNTXT: ', btnText);
 
   const getData = (categoria) => {
     axios
@@ -39,12 +46,13 @@ const Principal = ({ setItems,  items,  setPage, setText }) => {
 
   const handleSeeAll = () => {
     setSeeAll(!seeAll);
-    setBtnText(!btnText)
+    setBtnText(!btnText);
+    
   };
 
   const handleCategories = (e) => {
     setItems(e);
-    setText(`${textos.category} ${e}`)
+    setText(`${textos.category} ${e}`);
     getData(e);
     setPage('results');
     navigate('/results');
@@ -53,82 +61,91 @@ const Principal = ({ setItems,  items,  setPage, setText }) => {
   return (
     <Layout>
       <div className="container">
-        <div
-          className="categories"
-          // onClick={() => handleCategories('tours')}
-        >
-          <img src={predefTour} alt="hotel"></img>
-          <span>{textos.predefinedToursLabel}</span>
-        </div>
-        <div
-          className="categories"
-          // onClick={() => handleCategories('armar tour')}
-        >
-          <img src={setYourTour} alt="setYourTour"></img>
-          <span>{textos.buildMyTourLabel}</span>
-        </div>
-        <div
-          className="categories"
-          // onClick={() => handleCategories('hoteles')}
-        >
-          <img src={hotelImg} alt="hotel"></img>
-          <span>{textos.lodginLabel}</span>
-        </div>
-        <div
-          className="categories"
-          // onClick={() => handleCategories('restaurantes')}
-        >
-          <img src={restaurant} alt="restaurantes"></img>
-          <span>{textos.gastronomylabel}</span>
-        </div>
-        <div
-          className="categories"
-          // onClick={() => handleCategories('Actividades al Aire Libre')}
-        >
-          <img src={trips} alt="img"></img>
-          <span>{textos.outingLabel}</span>
-        </div>
-        <div
-          className="categories"
-          // onClick={() => handleCategories('transportes')}
-        >
-          <img src={transport} alt="transportes"></img>
-          <span>{textos.transportLabel}</span>
-        </div>
-      </div>
-
-      <div className="seeAllButtonDiv">
-        <button className="seeAllButton" onClick={handleSeeAll} >
-        {btnText === true ? 'Ver menos categorias': 'Ver mas categorias'}
-        </button>
-      </div>
-
-      {seeAll ? (
-        <>
-          <div className="moreServicesContainer">
-
-            <div className="categories" onClick={() => handleCategories('Espectaculos')}>
-              <img src={teatro} alt="espectaculos"></img>
-              <span>{textos.showsLabel}</span>
-            </div>
-
-            <div className="categories">
-              <img src={actividaesNocturnas} alt="actividaesNocturnas"></img>
-              <span>{textos.nightActivitiesLabel}</span>
-            </div>
-            <div className="categories" onClick={() => handleCategories('Servicios Esenciales')} >
-              <img src={serviciosEscenciales} alt="serviciosEscenciales"></img>
-              <span>{textos.esentialsServicesLabel}</span>
-            </div>
-            <div className="categories">
-              <img src={serviciosInfantiles} alt="serviciosInfantiles"></img>
-              <span>{textos.childActivities}</span>
-            </div>
+        <div className="containerCategories">
+          <div
+            className="categories"
+            // onClick={() => handleCategories('Tours Predefinidos')}
+          >
+            <img src={predefTour} alt="hotel"></img>
+            <span>{textos.predefinedToursLabel}</span>
           </div>
-        </>
-      ) : (
-        <></>
-      )}
+          <div
+            className="categories"
+            // onClick={() => handleCategories('Armar tour')}
+          >
+            <img src={setYourTour} alt="setYourTour"></img>
+            <span>{textos.buildMyTourLabel}</span>
+          </div>
+          <div
+            className="categories"
+            // onClick={() => handleCategories('Alojamiento')}
+          >
+            <img src={hotelImg} alt="hotel"></img>
+            <span>{textos.lodginLabel}</span>
+          </div>
+          <div
+            className="categories"
+            // onClick={() => handleCategories('Gastronomia')}
+          >
+            <img src={restaurant} alt="restaurantes"></img>
+            <span>{textos.gastronomylabel}</span>
+          </div>
+          <div
+            className="categories"
+            // onClick={() => handleCategories('Actividades al Aire Libre')}
+          >
+            <img src={trips} alt="img"></img>
+            <span>{textos.outingLabel}</span>
+          </div>
+          <div
+            className="categories"
+            // onClick={() => handleCategories('Transporte')}
+          >
+            <img src={transport} alt="transportes"></img>
+            <span>{textos.transportLabel}</span>
+          </div>
+        </div>
+
+        {seeAll || width > 811 ? (
+          <>
+            <div className="containerCategories">
+              <div
+                className="categories"
+                onClick={() => handleCategories('Espectaculos')}
+              >
+                <img src={teatro} alt="espectaculos"></img>
+                <span>{textos.showsLabel}</span>
+              </div>
+
+              <div className="categories">
+                <img src={actividaesNocturnas} alt="Actividaes Nocturnas"></img>
+                <span>{textos.nightActivitiesLabel}</span>
+              </div>
+              <div
+                className="categories"
+                onClick={() => handleCategories('Servicios Esenciales')}
+              >
+                <img
+                  src={serviciosEscenciales}
+                  alt="Servicios Esenciales"
+                ></img>
+                <span>{textos.esentialsServicesLabel}</span>
+              </div>
+              <div className="categories">
+                <img src={serviciosInfantiles} alt="Servicios Infantiles"></img>
+                <span>{textos.childActivities}</span>
+              </div>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+        <div className="seeAllButtonDiv">
+          <button className="seeAllButton" onClick={handleSeeAll}>
+            {btnText === true ? 'Ver menos categorias' : 'Ver mas categorias'}
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 };
