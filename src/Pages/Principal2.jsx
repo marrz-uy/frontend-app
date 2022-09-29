675a71bf7a096f5abe32a1696a9462afdffee0de
Ignacio <mateunacho6@gmail.com>
2022-09-29 14:57:28 -0300

    Arreglando bugs de userbar

---
+++ src/Pages/Principal2.jsx
@@ -0,0 +1,20 @@
+import { Layout } from '../Layout';
+import { useEffect, useContext } from 'react';
+import LenguageContext from '../Context/LenguageContext';
+import '../Css/Principal.css';
+import hotelImg from '../Assets/categoriesImages/hospedaje.png';
+import predefTour from '../Assets/categoriesImages/la-carretera.png';
+import setYourTour from '../Assets/categoriesImages/mosaico2.png';
+import restaurant from '../Assets/categoriesImages/fast-food 1.png';
+import trips from '../Assets/categoriesImages/summer-holidays 1.png';
+import transport from '../Assets/categoriesImages/bus.png';
+
+const Principal = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, voluptates! Consectetur neque porro ipsam harum aspernatur deserunt veritatis iste odit dolorum explicabo, quae autem perspiciatis magni eos expedita quisquam ullam?
+        </div>
+    );
+};
+
+export default Principal;
