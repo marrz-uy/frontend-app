73cd532b05e48e0ff509c0e13f6dca7c890741d4
martinrodfl <martinrodf@gmail.com>
2022-12-20 16:04:10 -0300

    Arregalndo responsivey posicion footer

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Nav from './Nav';
-import Footer from './Footer'
+import Footer from './Footer';
+import useScreenSize from '../Helpers/ScreenSize';
 import '../Css/Layout.css';
-
-const Layout = (props) => <div className="layout">{props.children}<Footer/></div>;
+const Layout = (props) => {
+	const {width, height } = useScreenSize()
+	console.log(width, height)
+  return (
+    <div className="layout"
+		style={{minHeigth:height}}
+		>
+      {props.children}
+      <Footer />
+    </div>
+  );
+};
 
 export { Layout, Nav, Footer };
