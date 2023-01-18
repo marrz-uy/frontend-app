1338aa0819a8c39f8715b585903601a47c01460b
martinrodfl <martinrodf@gmail.com>
2023-01-18 14:20:35 -0300

    Eliminar tour funcionando-ok

--- src/Pages/BuildTour/TourInit.jsx
+++ src/Pages/BuildTour/TourInit.jsx
@@ -7,6 +7,7 @@ import LenguageContext from '../../Context/LenguageContext';
 import { filtrarTraduccion } from '../../Helpers/FilterTranslate';
 import UserBar from '../../Pages/UserBar';
 import { handleUserBar } from '../../Helpers/HandUserBarClick';
+import trash from '../../Assets/trash.svg';
 import '../../Css/TourInit.css';
 
 const TourInit = ({
@@ -20,16 +21,16 @@ const TourInit = ({
   const { setActivePage } = useContext(PageContext);
   useEffect(() => {
     setPage('tourInit');
-    setActivePage('tourInit')
+    setActivePage('tourInit');
   }, [setPage, setActivePage]);
-  console.log('PAGE: ', page)
+  console.log('PAGE: ', page);
   const { http } = AuthUser();
 
   const Id = sessionStorage.getItem('id');
   const { traduccionesBD, lenguage } = useContext(LenguageContext);
 
   const [misTours, setMisTours] = useState();
-  useEffect(() => {
+  const getTours = () => {
     http
       .get(`/tourArmado/${Id}`, {})
       .then((response) => {
@@ -39,6 +40,10 @@ const TourInit = ({
         setMisTours(toursData);
       })
       .catch((error) => console.error(`Error en catch: ${error}`));
+  };
+
+  useEffect(() => {
+    getTours();
     // eslint-disable-next-line
   }, []);
 
@@ -49,13 +54,40 @@ const TourInit = ({
     str = str.substring(0, str.length - 3);
     return str;
   };
+
+  const deleteTour = (tourId) => {
+    http
+      .delete(`/tourArmado/${tourId}`, {})
+      .then((response) => {
+        console.log(
+          '%cTOURS DATA - toutInit:',
+          'color: violet;',
+          response?.data
+        );
+      })
+      .catch((error) => console.error(`Error en catch: ${error}`));
+  };
+
+  const handleDeleteTour = (e) => {
+    e.preventDefault();
+    console.log('eliminar tour: ');
+    console.log(e.target.id);
+    let tourID = e.target.id;
+    deleteTour(tourID);
+    getTours();
+  };
+
   return (
     <Layout>
       <div className="userbar-click" onClick={() => setUserBar(false)}></div>
       <div className="tourInit">
         <div className="contenedorTitulo">
-          <h2 className='textBlur'>{filtrarTraduccion(traduccionesBD, 'welcomeTo', lenguage)}: </h2>
-          <h1 className='textBlur'>{filtrarTraduccion(traduccionesBD, 'splashScreenTextSup', lenguage)}</h1>
+          <h2 className="textBlur">
+            {filtrarTraduccion(traduccionesBD, 'welcomeTo', lenguage)}:{' '}
+          </h2>
+          <h1 className="textBlur">
+            {filtrarTraduccion(traduccionesBD, 'splashScreenTextSup', lenguage)}
+          </h1>
         </div>
         <div className="pageText">
           <h3>{filtrarTraduccion(traduccionesBD, 'yourOwnTours', lenguage)}</h3>
@@ -65,43 +97,66 @@ const TourInit = ({
             <div className="btnBuildTour">
               <Link to="/buildTour">
                 <div className="seccionCrearTour">
-                <span className='icons'>🚍</span><p>{filtrarTraduccion(traduccionesBD, 'createTour', lenguage)}</p>
+                  <span className="icons">🚍</span>
+                  <p>
+                    {filtrarTraduccion(traduccionesBD, 'createTour', lenguage)}
+                  </p>
                 </div>
               </Link>
             </div>
           </div>
           <div className="seccionVerMisTours">
             <div className="contenedorTitulo">
-              <h1 className='textBlur'>{filtrarTraduccion(traduccionesBD, 'seeMyTours', lenguage)} <span className='icons'></span>🚡</h1>
+              <h1 className="textBlur">
+                {filtrarTraduccion(traduccionesBD, 'seeMyTours', lenguage)}{' '}
+                <span className="icons"></span>🚡
+              </h1>
             </div>
             <div className="pageText">
-              <h3 className='textBlur'>
-               {filtrarTraduccion(traduccionesBD, 'previouslyTours', lenguage)}
-             </h3>
+              <h3 className="textBlur">
+                {filtrarTraduccion(traduccionesBD, 'previouslyTours', lenguage)}
+              </h3>
             </div>
           </div>
           <div className="tourList">
             {misTours?.map((tour) => {
               return (
                 <details key={tour.id}>
+                  <div></div>
                   <summary>
                     <span>{tour.nombreTour}</span>
                   </summary>
                   <div className="myToursCard">
                     <div>
-                      <span>🕛</span>{filtrarTraduccion(traduccionesBD, 'beginsAt', lenguage)}{' '} {hora(tour.horaInicioTour)} hs
+                      <span>🕛</span>
+                      {filtrarTraduccion(
+                        traduccionesBD,
+                        'beginsAt',
+                        lenguage
+                      )}{' '}
+                      {hora(tour.horaInicioTour)} hs
                     </div>
                     <div className="cardContent">
                       {' '}
                       {tour?.tour_items?.map((tourItem) => {
                         return (
                           <div key={tourItem.puntoInteresId}>
-                            <li>{tourItem.puntos_interes.Nombre}</li>
+                            <li className="puntoInteresLi">
+                              {tourItem.puntos_interes.Nombre}
+                            </li>
                           </div>
                         );
                       })}
                     </div>
                   </div>
+                  <span className="deleteIcon">
+                    <img
+                      src={trash}
+                      alt="trashCan"
+                      id={tour.id}
+                      onClick={handleDeleteTour}
+                    ></img>
+                  </span>
                 </details>
               );
             })}
