20a19d4bc4f0912417046d9cd12759e8210f0931
martinrodfl <martinrodf@gmail.com>
2023-01-21 19:27:35 -0300

    Interfaz de notificaciones funcionando

--- src/Components/TourComponents/NoTourMsg.jsx
+++ src/Components/TourComponents/NoTourMsg.jsx
@@ -4,7 +4,7 @@ const NoTourMsg = (props) => {
   return (
     <div className="divNoTour">
       <div className="divNoTourMsg">
-        <h4 className="messageNoTour">{props.message}</h4>
+        <h2 className="messageNoTour">{props.message}</h2>
       </div>
       <div className="divImage">
         <img src={props.image} alt="img"></img>
