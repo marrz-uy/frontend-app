7fa1a0c8aafe3d3d5e72518b7e835bf03aa85c54
martinrodfl <martinrodf@gmail.com>
2023-07-19 19:37:05 -0300

    cambiando ip para backend 192.168.1.4:8000

--- src/Components/AuthUser.jsx
+++ src/Components/AuthUser.jsx
@@ -100,7 +100,7 @@ export default function AuthUser() {
     setUserfavourites(userfavourites);
   };
   const http = axios.create({
-    baseURL: 'http://192.168.3.3:8000/api',
+    baseURL: 'http://192.168.1.4:8000/api',
     headers: {
       'Content-type': 'application/json',
       Authorization: `Bearer ${token}`,
